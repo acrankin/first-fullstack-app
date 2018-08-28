6d98ed70da97db07a3eeefbb4fcdbd6341bc8647
Alex Rankin <alex.c.rankin@gmail.com>
2018-08-28 15:53:45 -0700

    data showing in browser

---
+++ app/src/components/drinks/Drink.vue
@@ -0,0 +1,19 @@
+<template>
+  <li>
+    <p>{{ drink.name }}</p>
+    <p>{{ drink.baseSpirit }}</p>
+  </li>
+    
+</template>
+
+<script>
+export default {
+  props: {
+    drink: Object
+  }
+};
+</script>
+
+<style>
+
+</style>
--- app/src/components/drinks/Drinks.vue
+++ app/src/components/drinks/Drinks.vue
@@ -1,10 +1,40 @@
 <template>
-  <h2>all my drinksss</h2>
-    
+  <section>
+    <h2>all my drinksss</h2>
+    <ul class="list">
+      <Drink
+        v-for="(drink, index) in drinks"
+        :key="index"
+        :drink="drink"
+      />
+    </ul>  
+  </section>  
 </template>
 
 <script>
+import api from '../../services/api';
+import Drink from './Drink.vue';
+
 export default {
+  data() {
+    return {
+      drinks: null
+    };
+  },
+  created() {
+    api.getDrinks()
+      .then(drinks => {
+        this.drinks = drinks;
+        console.log(this.drinks);
+      });
+  },
+  components: {
+    Drink
+    //AddDrink
+  },
+  methods: {
+    //handleAdd
+  }
 
 };
 </script>
